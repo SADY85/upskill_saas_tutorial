9c7bbd4f6003cfd3fdffcfe9c27745886d72ddcd
Alex Young <stephan.alex.young@gmail.com>
2017-01-22 18:17:27 +0000

    replaced contact data migration files

--- db/migrate/20170102220326_add_timestamps_to_contacts.rb
+++
@@ -1,6 +0,0 @@
-class AddTimestampsToContacts < ActiveRecord::Migration[5.0]
-  def change_table
-    add_column(:users, :created_at, :datetime)
-    add_column(:users, :updated_at, :datetime)
-  end
-end
